2b9a84d3daf5f40a7cf15c9fc9fc0565ea738b28
sunny-yo <dmsfh3@gmail.com>
2022-03-21 00:39:05 +0900

    fix: main page 검색
    검색 - 검색 결과 설정 필요, 결과 표시하는 거 필요

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import Navbar from '../components/container/Navbar';
@@ -7,13 +8,15 @@ import _place from '../state/redux/place/placeSelector';
 function Main() {
   const place = useSelector(_place);
   const mapRef = useRef();
+  const searchRef = useRef();
+  const searchFormRef = useRef();
 
-  const mapOptions = {
-    center: new window.kakao.maps.LatLng(37.566, 126.9786),
-    level: 3,
-  };
-
+  // 지도, 지도 위 마커 표시
   useEffect(() => {
+    const mapOptions = {
+      center: new window.kakao.maps.LatLng(37.566, 126.9786),
+      level: 3,
+    };
     const map = new window.kakao.maps.Map(mapRef.current, mapOptions);
 
     place.forEach((onePlace) => {
@@ -28,7 +31,6 @@ function Main() {
       };
 
       const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
       const marker = new window.kakao.maps.Marker({
         position: new window.kakao.maps.LatLng(latitude, longitude),
         title: placeName,
@@ -38,14 +40,39 @@ function Main() {
 
       marker.setMap(map);
     });
+
     return () => {};
   }, []);
+
+  // 검색 - 검색 결과 설정 필요, 결과 표시하는 거 필요
+  const places = new kakao.maps.services.Places();
+  const searchCallback = (result, status) => {
+    if (status === kakao.maps.services.Status.OK) {
+      console.log(result);
+    }
+  };
+  const searchPlace = (e) => {
+    e.preventDefault();
+    if (searchRef.current.value === '') return;
+    places.keywordSearch(searchRef.current.value, searchCallback);
+
+    searchFormRef.current.reset();
+  };
 
   return (
     <LayoutWrapper>
       <Navbar title="로고" />
-      <form className="flex mt-[12px] mx-[20px] px-[20px] py-[11px] mb-[8px] bg-[#E7E6FE] rounded-[14px]">
-        <input type="text" placeholder="가고싶은 곳을 검색해보세요!" className="flex-1 bg-[#E7E6FE] text-black text-[14px] leading-[17px]" />
+      <form
+        ref={searchFormRef}
+        onSubmit={searchPlace}
+        className="flex mt-[12px] mx-[20px] px-[20px] py-[11px] mb-[8px] bg-[#E7E6FE] rounded-[14px]"
+      >
+        <input
+          ref={searchRef}
+          type="text"
+          placeholder="가고싶은 곳을 검색해보세요!"
+          className="flex-1 bg-[#E7E6FE] text-black text-[14px] leading-[17px]"
+        />
         <button type="button">검색</button>
       </form>
       <div ref={mapRef} className="w-full h-full" />
