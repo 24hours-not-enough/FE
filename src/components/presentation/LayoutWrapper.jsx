63a56e7add7b7f6edabd0c51b0921cc9090dc31e
sunny-yo <dmsfh3@gmail.com>
2022-03-24 03:10:39 +0900

    feat: LayoutWrapper overflow 스타일옵션 추가

--- src/components/presentation/LayoutWrapper.jsx
+++ src/components/presentation/LayoutWrapper.jsx
@@ -2,12 +2,23 @@ import { memo } from 'react';
 
 import useLayout from '../../shared/useLayout';
 
-function LayoutWrapper({ children }) {
+function LayoutWrapper({ children, overflow }) {
   const { pc } = useLayout();
+
+  const overflowType = () => {
+    switch (overflow) {
+      case 'auto':
+        return 'overflow-auto scrollbar-hide';
+      case 'hide':
+        return 'overflow-hidden';
+      default:
+        return 'overflow-auto scrollbar-hide';
+    }
+  };
 
   if (!pc) {
     return (
-      <div className="bg-main-background w-full h-full overflow-auto scrollbar-hide">
+      <div className={`bg-main-background w-full h-full ${overflowType}`}>
         {children}
       </div>
     );
